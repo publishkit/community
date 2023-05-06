6a44cd2848940bb826c5d48e177c53b0af4396a4
louis <louis.grellet@gmail.com>
2023-05-06 20:14:16 +0200

    clean

--- plugins/fancybox.js
+++ plugins/fancybox.js
@@ -1,40 +1,31 @@
-// author : zdaar
-// email : daveloper@protonmail.com
+// author: zdaar
+// email: daveloper@protonmail.com
 return class FancyBoxPlugin extends BasePlugin {
-    constructor(id, options) {
-        super(id, options);
-    }
-
-    deps = [
-        "https://cdn.jsdelivr.net/npm/@fancyapps/ui@5.0/dist/fancybox/fancybox.umd.js",
-        "https://cdn.jsdelivr.net/npm/@fancyapps/ui@5.0/dist/fancybox/fancybox.css",
+  constructor(id, options) {
+    super(id, options);
+    this.deps = [
+      "https://cdn.jsdelivr.net/npm/@fancyapps/ui@5.0/dist/fancybox/fancybox.umd.js",
+      "https://cdn.jsdelivr.net/npm/@fancyapps/ui@5.0/dist/fancybox/fancybox.css",
     ];
+  }
 
-    init = async () => {
-        console.log("Fancybox plugin initialized");
-        return true;
-    };
+  transform = async () => {
+    const { $dom } = window;
+    const { log } = this
+    // this plugin only transforms images in the content area
+    const images = $dom.body.find(".ui-content img");
 
-    render = async () => {
-    };
+    images.each(function () {
+      const img = $(this);
+      const src = img.attr("src");
+      img.wrap(`<a href="${src}" data-fancybox="image-gallery"></a>`);
+    });
+  };
 
-    transform = async () => {
-        const { $dom } = window;
-        // this plugin only transforms images in the content area
-        const images = $dom.body.find(".ui-content img");
-
-        images.each(function () {
-            const img = $(this);
-            const src = img.attr("src");
-            img.wrap(`<a href="${src}" data-fancybox="image-gallery"></a>`);
-            console.log("Transformed image:", img);
-        });
-    };
-
-    bind = async () => {
-        Fancybox.bind('[data-fancybox="image-gallery"]', {
-            'resizeDuration': 200,
-            'wrapAround': true,
-        });
-    };
+  bind = async () => {
+    Fancybox.bind('[data-fancybox="image-gallery"]', {
+      resizeDuration: 200,
+      wrapAround: true,
+    });
+  };
 };
